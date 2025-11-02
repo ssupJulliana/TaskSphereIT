f5c1baf5c879623fdaff0b15faf6435a06f5e7ae
ShaneKianCastillo <castillogohan609@gmail.com>
2025-11-02 08:42:23 +0800

    Second Attept for Database Transfer

--- tasksphereit/src/config/firebase.js
+++ tasksphereit/src/config/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCJ6oPXpjNH02Cas37Vt6VbZ7CSrHqk6Lk",
-  authDomain: "tasksphereit-b53c8.firebaseapp.com",
-  projectId: "tasksphereit-b53c8",
-  storageBucket: "tasksphereit-b53c8.firebasestorage.app",
-  messagingSenderId: "638410355707",
-  appId: "1:638410355707:web:2773f33db240ae1201d15e",
-  measurementId: "G-ZQTD7KEX9K",
+  apiKey: "AIzaSyAr8KuLBOOs0JcZJXCiy-uliI4NC49-_K0",
+  authDomain: "tasksphereit-6d23f.firebaseapp.com",
+  projectId: "tasksphereit-6d23f",
+  storageBucket: "tasksphereit-6d23f.firebasestorage.app",
+  messagingSenderId: "478108689425",
+  appId: "1:478108689425:web:edaaf3cc050165573cbdb0",
+  measurementId: "G-SXCXT1NFVN",
 };
 
 const app = initializeApp(firebaseConfig);
